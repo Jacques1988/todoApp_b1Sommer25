c29c3d8a258ebabc385fb7073f85e65a279d8684
Jacques <jacquesvanluyck@gmail.com>
2026-01-23 09:29:26 +0100

    update teaser component, install bootstrap

--- src/app/filter/filter.ts
+++ src/app/filter/filter.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { Teaser } from '../shared/components/teaser/teaser';
 
 @Component({
   selector: 'app-filter',
-  imports: [],
+  imports: [Teaser],
   templateUrl: './filter.html',
   styleUrl: './filter.css',
 })
-export class Filter {
-
-}
+export class Filter {}
--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { Teaser } from '../shared/components/teaser/teaser';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [Teaser],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {
-
-}
+export class Header {}
