6a32a91249e67ad6ffbf5612430499bfe52105a6
Jacques <jacquesvanluyck@gmail.com>
2026-01-26 11:57:17 +0100

    build responsive design to filter component, add button as module component, fixes #9 fixes #7

--- src/app/shared/components/main-button/main-button.ts
+++ src/app/shared/components/main-button/main-button.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-main-button',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './main-button.css',
 })
 export class MainButton {
-
+  btnText = input<string>();
 }
